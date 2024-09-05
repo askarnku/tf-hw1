9371aee9981e6af371767d07fe68a076d492228f
Askar Mamytov <askar.mamytov201@gmail.com>
2024-09-05 11:16:51 -0500

    adds metadata to dynamodb.tf resource

--- dynamodb.tf
+++ dynamodb.tf
@@ -2,6 +2,10 @@ resource "aws_dynamodb_table" "terraform_locks_hw_tf1" {
   name         = "terraform-locks-hw-tf1" 
   billing_mode = "PAY_PER_REQUEST" 
   hash_key     = "LockID"
+
+  lifecycle {
+    prevent_destroy = true
+  }
  
   attribute { 
     name = "LockID" 
