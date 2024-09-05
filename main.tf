9eca129435c2ea23de8b2ec6c50f5e5b4cf8f2ba
Askar Mamytov <askar.mamytov201@gmail.com>
2024-09-05 11:11:27 -0500

    adds dynamodb state lock

--- main.tf
+++ main.tf
@@ -2,10 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#yourNametentech-first-bucket , yourNametentech-second-bucket ,yourNametentech-third-bucket
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-46dc7020e6341676"
+    key = "tf-hw1/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locks-hw-tf1"
+    encrypt = false
+  }
+}
 
 resource "aws_s3_bucket" "first_bucket" {
-  bucket = "tentek-askar-first-bucket"
+  bucket = "tentek-askar-bucket-one"
   force_destroy = true
   tags = {
     Name = "My first bucket"
@@ -13,7 +21,7 @@ resource "aws_s3_bucket" "first_bucket" {
 }
 
 resource "aws_s3_bucket" "second_bucket" {
-  bucket = "tentek-askar-second-bucket"
+  bucket = "tentek-askar-bucket-two"
   force_destroy = true
   tags = {
     Name = "My second bucket"
@@ -21,9 +29,21 @@ resource "aws_s3_bucket" "second_bucket" {
 }
 
 resource "aws_s3_bucket" "third_bucket" {
-  bucket = "tentek-askar-third-bucket"
+  bucket = "tentek-askar-bucket-three"
   force_destroy = true
   tags = {
     Name = "My third bucket"
   }
+}
+
+resource "aws_vpc" "vpc_tentek_one" {
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_vpc" "vpc_tentek_two" {
+  cidr_block = "10.1.0.0/24"
+}
+
+resource "aws_vpc" "vpc_tentek_three" {
+  cidr_block = "10.2.0.0/24"
 }
